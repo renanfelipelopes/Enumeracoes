88f9c576178ecda1828e6cf3ce9b9ad06294ac48
Renan Lopes <renanlopes.ads@gmail.com>
2021-09-25 23:02:10 -0300

    practicing

---
+++ Enumeracoes/Program.cs
@@ -0,0 +1,30 @@
+﻿using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+using System;
+
+namespace Enumeracoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            //Convert to string
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            //Convert to Enum
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+            Console.WriteLine(order);
+        }
+    }
+}
